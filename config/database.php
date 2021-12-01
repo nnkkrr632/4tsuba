d6b16d6e06ceeca8b98ccea8d34bbcdc32f6e0cc
nnkkrr632 <nnkkrr632@gmail.com>
2021-12-01 17:35:58 +0900

    config:  redisを利用するための設定変更(redisクライアントとしてpredisを使用)

--- config/database.php
+++ config/database.php
@@ -119,11 +119,13 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT'),
+        'cluster' => false,
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
+            'parameters' => ['password' => env('REDIS_PASSWORD', null)],
         ],
 
         'default' => [
@@ -132,6 +134,8 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            //追加
+            'read_write_timeout' => 60,
         ],
 
         'cache' => [
